8c07e46b4027df33cfd64ac399d23d65f9a6f057
Ariel <ariel.kuzminski@gmail.com>
2018-12-21 00:26:54 +0100

    Add shutdownPC function

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const delayArgument = process.argv[2];
 let timer = delayArgument;
 
 const interval = setInterval(() => {
-    console.log(`Komputer wyłączy się za : ${timer} sekund`);
-    if (timer === 0) {
-        clearInterval(interval);
-    }
-    timer--;
-}, 1000)
+  console.log(`Komputer wyłączy się za : ${timer} sekund`);
+  if (timer === 0) {
+    clearInterval(interval);
+    shutdownPC();
+  }
+  timer--;
+}, 1000);
+
+const shutdownPC = () => {
+  nodeCmd.get('shutdown -s -t 00', (err, data, stderr) => console.log(err, data, stderr));
+};
