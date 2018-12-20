b08eb30f011cb968ed6651f2027fc4ab3ebe65ce
Ariel <ariel.kuzminski@gmail.com>
2018-12-21 00:23:31 +0100

    initialize interval function and process argument from console

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 const nodeCmd = require('node-cmd');
+
+const delayArgument = process.argv[2];
+let timer = delayArgument;
+
+const interval = setInterval(() => {
+    console.log(`Komputer wyłączy się za : ${timer} sekund`);
+    if (timer === 0) {
+        clearInterval(interval);
+    }
+    timer--;
+}, 1000)
